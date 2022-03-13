c07e55045b5ab28e255adf71438ebe4cd243b4f7
lucaspanwang <wang.pan1@northeastern.edu>
2022-03-13 11:49:31 -0700

    Update

--- team10/src/components/Search/Search.js
+++ team10/src/components/Search/Search.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./Search.css";
 import { cities } from "./Search.constant";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-function createData(area,
+function createData(
+  area,
   beef,
   diary,
   egg,
@@ -18,9 +19,8 @@ function createData(area,
   mushroom,
   pork,
   chicken,
-  otherpoultry) {
-
-
+  otherpoultry
+) {
   return {
     area,
     beef,
@@ -31,16 +31,15 @@ function createData(area,
     mushroom,
     pork,
     chicken,
-    otherpoultry
+    otherpoultry,
   };
 }
 
-const rows = [
-  createData('Burnaby', 20, 0, 0, 0, 0, 0, 0, 0, 0),
-];
+const rows = [createData("Burnaby", 20, 0, 0, 0, 0, 0, 0, 0, 0)];
 
 export default function Search() {
   const [city, setCity] = useState("City");
+  const [showTable, setShowTable] = useState(false);
   useEffect(() => {
     const cityForm = document.querySelector("#city");
     for (let city of cities) {
@@ -66,11 +65,12 @@ export default function Search() {
 
   const showCity = (e) => {
     e.preventDefault();
-    fetch(`http://localhost:4000/citystatistics/${city}`)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-      });
+    // fetch(`http://localhost:4000/citystatistics/${city}`)
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+    setShowTable(true);
   };
 
   return (
@@ -86,50 +86,54 @@ export default function Search() {
               ></select>
             </form>
             <form>
-              <button className="suf-submit" onClick={showCity}>Search</button>
+              <button className="suf-submit" onClick={showCity}>
+                Search
+              </button>
             </form>
           </div>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Area</TableCell>
-                  <TableCell align="right">Beef</TableCell>
-                  <TableCell align="right">Diary</TableCell>
-                  <TableCell align="right">Egg</TableCell>
-                  <TableCell align="right">Fruitnut</TableCell>
-                  <TableCell align="right">Vegetable</TableCell>
-                  <TableCell align="right">Mushroom</TableCell>
-                  <TableCell align="right">Pork</TableCell>
-                  <TableCell align="right">Chicken</TableCell>
-                  <TableCell align="right">Other-poultry</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.area}
-                    </TableCell>
-                    <TableCell align="right">{row.beef}</TableCell>
-                    <TableCell align="right">{row.diary}</TableCell>
-                    <TableCell align="right">{row.egg}</TableCell>
-                    <TableCell align="right">{row.fruitnut}</TableCell>
-                    <TableCell align="right">{row.vegetable}</TableCell>
-                    <TableCell align="right">{row.mushroom}</TableCell>
-                    <TableCell align="right">{row.pork}</TableCell>
-                    <TableCell align="right">{row.chicken}</TableCell>
-                    <TableCell align="right">{row.otherpoultry}</TableCell>
+          {showTable && (
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Area</TableCell>
+                    <TableCell align="right">Beef</TableCell>
+                    <TableCell align="right">Diary</TableCell>
+                    <TableCell align="right">Egg</TableCell>
+                    <TableCell align="right">Fruitnut</TableCell>
+                    <TableCell align="right">Vegetable</TableCell>
+                    <TableCell align="right">Mushroom</TableCell>
+                    <TableCell align="right">Pork</TableCell>
+                    <TableCell align="right">Chicken</TableCell>
+                    <TableCell align="right">Other-poultry</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.area}
+                      </TableCell>
+                      <TableCell align="right">{row.beef}</TableCell>
+                      <TableCell align="right">{row.diary}</TableCell>
+                      <TableCell align="right">{row.egg}</TableCell>
+                      <TableCell align="right">{row.fruitnut}</TableCell>
+                      <TableCell align="right">{row.vegetable}</TableCell>
+                      <TableCell align="right">{row.mushroom}</TableCell>
+                      <TableCell align="right">{row.pork}</TableCell>
+                      <TableCell align="right">{row.chicken}</TableCell>
+                      <TableCell align="right">{row.otherpoultry}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
         </section>
       </div>
     </div>
-  )
+  );
 }
