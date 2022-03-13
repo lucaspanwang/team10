326382b398ae1223a646c30e7cc69c060f81a4de
Amadus <790494168@qq.com>
2022-03-12 23:52:04 -0800

    Home fetch data

--- team10/src/components/Home/Home.js
+++ team10/src/components/Home/Home.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("******");
+    fetch(`http://localhost:4000/allrequests`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setData(res);
+      });
+  }, []);
+
+  return <p></p>;
 }
